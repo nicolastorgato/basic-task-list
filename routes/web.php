39658af4b22f7f39e3eadd4eca5577d35f23064c
storgatonicola <storgatonicola@provincia.treviso.it>
2020-02-27 11:29:18 +0100

    Tasks List, Task Create & Task Delete

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,54 @@
 |
 */
 
+
+/**
+ * Display all tasks
+ */
+
+use App\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+
+
+/**
+ * Add a new task
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+
+});
+
+
+
+/**
+ * Delete an existing task
+ */
+Route::delete('/task/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
+    
+    return redirect('/');
 });
