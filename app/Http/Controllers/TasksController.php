f179607f5079a64fdf61874cc2af8b9180fd29e6
storgatonicola <storgatonicola@provincia.treviso.it>
2020-02-28 13:21:23 +0100

    TasksController added.

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TasksController extends Controller
+{
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::orderBy('created_at', 'asc')->get();
+
+        return view('tasks', [
+            'tasks' => $tasks
+        ]);
+    }
+
+
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'description' => 'max:255'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->description = $request->description;
+        $task->save();
+
+        return redirect('/');
+
+    }
+
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Task::findOrFail($id)->delete();
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,55 +12,24 @@
 */
 
 
+
 /**
  * Display all tasks
  */
-
-use App\Task;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-
-Route::get('/', function () {
-    $tasks = Task::orderBy('created_at', 'asc')->get();
-
-    return view('tasks', [
-        'tasks' => $tasks
-    ]);
-});
+Route::get('/','TasksController@index');
 
 
 
 /**
  * Add a new task
  */
-Route::post('/task', function (Request $request) {
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-        'description' => 'max:255'
-    ]);
 
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
+Route::post('/task', 'TasksController@store');
 
-    $task = new Task;
-    $task->name = $request->name;
-    $task->description = $request->description;
-    $task->save();
-
-    return redirect('/');
-
-});
 
 
 
 /**
  * Delete an existing task
  */
-Route::delete('/task/{id}', function ($id) {
-    Task::findOrFail($id)->delete();
-    
-    return redirect('/');
-});
+Route::delete('/task/{id}', 'TasksController@destroy');
